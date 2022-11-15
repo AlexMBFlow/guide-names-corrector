af464a4338e6642a93371815bd8fe5939e69ecde
Alex <maltzev.alexandr@gmail.com>
2022-11-15 19:26:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const stringCorrector = (text) => {
-    let result;
-    let firstWord = text[0].toLowerCase();
+  let result;
+  let firstWord = text[0].toLowerCase();
+
+  let replasedText = text
+    .replaceAll(" ", "_")
+    .replaceAll(",", "")
+    .replaceAll("'", "")
+    .replaceAll(".webm", "")
+    .replaceAll(".mp4", "");
+  result = firstWord + replasedText.substr(1);
+  console.log(result);
+};
+
+stringCorrector("Speak up, don't be shy.mp4.webm");
+
   
-    let replasedText = text
-      .replaceAll(" ", "_")
-      .replaceAll(",", "")
-      .replaceAll("'", "")
-      .replaceAll(".webm", "");
-    result = firstWord + replasedText.substr(1);
-    console.log(result);
-  };
-  
-  //stringCorrector("Speak up, don't be shy.webm");
+  //stringCorrector("Speak up, don't be shy.mp4.webm");
   //Result: speak_up_dont_be_shy 
   
